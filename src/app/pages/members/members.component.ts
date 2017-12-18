e4ca6b0934923508ffb42896bc58d97386e61ba0
woohaoshu <woohaoshu@gmail.com>
2017-12-18 10:00:21 +0800

    Add the scdm logo and reset the order of the nav.

--- src/app/pages/members/members.component.ts
+++ src/app/pages/members/members.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ScrollSpyService } from 'ngx-scrollspy';
 declare var $: any; //使用jQuery
 
 @Component({
@@ -11,13 +10,10 @@ export class MembersComponent implements OnInit {
   public affixTopMargin = 0;//固定在顶部参数
   
 
-  constructor(private scrollSpyService: ScrollSpyService) {}
+  constructor() {}
 
   ngOnInit() {
     this.affixTopMargin = $(window).height();
-    // this.scrollSpyService.getObservable('window').subscribe((e: any) => {
-		// 	console.log('ScrollSpy::window: ', e);
-		// });
   }
 
   public goTo(anchor: string) {
--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any; //使用jQuery
 
 @Component({
   selector: 'scdm-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+  public affixTopMargin = 0;//固定在顶部参数
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.affixTopMargin = $(window).height();
+  }
+
+  public goTo(anchor: string) {
+    setTimeout(() => {
+      (<any>document).querySelector('#' + anchor).scrollIntoView();
+    });
   }
 
 }
