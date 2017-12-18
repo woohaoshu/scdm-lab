1fcd97643d5d9908bd7b4976ff4af3348e7419c9
woohaoshu <woohaoshu@gmail.com>
2017-12-18 13:54:35 +0800

    Added the content of papers and projects, seted up the framefork of the news and meetings page.

--- src/app/pages/papers/papers.component.ts
+++ src/app/pages/papers/papers.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any; //使用jQuery
 
 @Component({
   selector: 'scdm-papers',
   templateUrl: './papers.component.html',
-  styleUrls: ['./papers.component.css']
+  styleUrls: ['./papers.component.scss']
 })
 export class PapersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  public affixTopMargin = 0;//固定在顶部参数
+  
+    constructor() {}
+  
+    ngOnInit() {
+      this.affixTopMargin = $(window).height();
+    }
+  
+    public goTo(anchor: string) {
+      setTimeout(() => {
+        (<any>document).querySelector('#' + anchor).scrollIntoView();
+      });
+    }
 }
